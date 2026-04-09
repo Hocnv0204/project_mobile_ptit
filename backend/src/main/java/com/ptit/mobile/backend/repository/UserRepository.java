dd6963ca4bf7fc7274d59bc19fb70502672bf85a
Hoc Nguyen <nguyenhh1102@gmail.com>
2026-04-09 16:58:18 +0700

    update config

--- backend/src/main/java/com/ptit/mobile/backend/repository/UserRepository.java
+++ backend/src/main/java/com/ptit/mobile/backend/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.ptit.mobile.backend.repository;
 import com.ptit.mobile.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Integer, User > {
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
